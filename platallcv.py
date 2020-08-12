111643cc739f6fe6ddf6384e754b1a5f59f8573e
Malaga Tejaswini <tejutejaswini156@gmail.com>
2020-08-12 18:08:47 +0530

    Delete platallcv.py

--- platallcv.py
+++
@@ -1,9 +0,0 @@
-import COVID19Py
-import matplotlib.pyplot as plt
-covid19=COVID19Py.COVID19()
-data=covid19.getAll(timelines=True)
-virusdata=dict(data["latest"])
-names=list(virusdata.keys())
-values=list(virusdata.values())
-plt.bar(range(len(virusdata)),values,tick_label=names)
-plt.show()
