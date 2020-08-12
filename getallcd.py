7dec23289b0e2235e857d8302f2eadc90301df7b
Malaga Tejaswini <tejutejaswini156@gmail.com>
2020-08-12 18:06:28 +0530

    Delete getallcd.py

--- getallcd.py
+++
@@ -1,7 +0,0 @@
-import COVID19Py
-covid19=COVID19Py.COVID19()
-data=covid19.getAll(timelines=True)
-virusdata=dict(data["latest"])
-names=list(virusdata.keys())
-values=list(virusdata.values())
-print(virusdata)
