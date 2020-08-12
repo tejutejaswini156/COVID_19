01219d07d4fed361cafa7711cb24cf703578f86f
Malaga Tejaswini <tejutejaswini156@gmail.com>
2020-08-12 18:13:15 +0530

    Update getcountrydata.py

--- getcountrydata.py
+++ getcountrydata.py
@@ -1,6 +1,4 @@
 import COVID19Py
 covid19=COVID19Py.COVID19()
 location=covid19.getLocationByCountryCode("IN")
-loc_data=location[0]
-virusdata=dict(loc_data["latest"])
-print(virusdata)
+print(location)
